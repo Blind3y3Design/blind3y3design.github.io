7c735cffc034f543f5cf30cb97fcd0811f9f9689
Adam Sedwick <asedwick@blind3y3design.com>
2023-08-31 01:15:31 -0500

    mobile menu and year split for work and posts

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,10 @@
-var myElement = document.querySelector(".headroom");
-var headroom  = new Headroom(myElement);
-headroom.init();
+// var myElement = document.querySelector(".headroom");
+// var headroom  = new Headroom(myElement);
+// headroom.init();
+
+function toggleMobileMenu() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('mobile-menu--open');
+}
+
+document.querySelector('.mobile-menu-trigger').addEventListener("click", toggleMobileMenu);
